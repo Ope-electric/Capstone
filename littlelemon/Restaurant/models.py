7e6cef59fc9176a3445702c13877d1aa70353515
Ope-electric <afolabiyusuf985@gmail.com>
2026-07-21 18:16:49 +0100

    Add Menu and Booking models

--- littlelemon/Restaurant/models.py
+++ littlelemon/Restaurant/models.py
@@ -1,3 +1,15 @@
+from datetime import datetime
+
 from django.db import models
 
 # Create your models here.
+
+class Menu(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    inventory = models.IntegerField()
+    
+class Booking(models.Model):
+    name = models.CharField(max_length=255)
+    No_of_guests = models.IntegerField()
+    BookingDate = models.DateField()
--- littlelemon/littlelemon/settings.py
+++ littlelemon/littlelemon/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Restaurant',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
@@ -49,7 +51,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'Restaurant.urls'
+ROOT_URLCONF = 'littlelemon.urls'
 
 TEMPLATES = [
     {
